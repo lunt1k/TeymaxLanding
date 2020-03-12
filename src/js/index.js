dbe8536c9104fcc7e33922fe797759b391f15030
Maks Luniak <pi59_lmo@student.ztu.edu.ua>
2020-03-12 17:58:21 +0200

    First block was created

---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+import '../style/style.css';
+
+console.log("Welcome! Greetings from app.js. Let's learn Webpack2");
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin= require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+module.exports = {
+  entry: [
+    './src/js/index.js',
+  ],
+  output: {
+    filename: './js/bundle.js'
+  },
+  devtool: "source-map",
+  module: {
+    rules: [{
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src/js'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.svg/,
+        use: {
+            loader: 'svg-url-loader',
+            options: {}
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({  // Also generate a test.html
+        filename: 'index.html',
+        template: 'src/index.html'
+    }),
+    new CopyWebpackPlugin([{
+        from: './src/fonts',
+        to: './fonts'
+      },
+      {
+        from: './src/favicon',
+        to: './favicon'
+      },
+      {
+        from: './src/img',
+        to: './img'
+      },
+    ]),
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    })
+  ]
+};
