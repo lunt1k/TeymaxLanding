550c9abbde94f603ce813de7662b61256673f077
Maks Luniak <mluniak15@gmail.com>
2020-04-09 23:44:21 +0300

    Added animation on mobile to navbar opening, fixed scroll behavior

--- src/js/elements.js
+++ src/js/elements.js
@@ -4,10 +4,14 @@ const elements = {
   nextButton: document.getElementById('sliderNextButton'),
   controlButtons: document.querySelector('slider-controls-block'),
   menuBtn: document.querySelector('.navbar-toggle'),
+  navbar: document.querySelector('.navbar'),
   navItemsBlock: document.querySelector('.navbar-collapse'),
   navItems: document.querySelectorAll('.navbar-collapse__item'),
   navContactLinks: document.querySelector('.navbnavbar-contact-links'),
-  navAdress: document.querySelector('.navbar-tel-address')
+  navAdress: document.querySelector('.navbar-tel-address'),
+  navLinks: document.querySelectorAll('.navbar-collapse__link'),
+  sections: document.querySelectorAll('section'),
+  contactUsSection: document.getElementById('contact_us')
 }
 
 export default elements
--- src/js/scrollspy.js
+++ src/js/scrollspy.js
@@ -1,17 +1,5 @@
-const makeNavLinksSmooth = () => {
-  const navLinks = document.querySelectorAll('.navbar-collapse__link')
-
-  for (let n in navLinks) {
-    if (navLinks.hasOwnProperty(n)) {
-      navLinks[n].addEventListener('click', (e) => {
-        e.preventDefault()
-        document.querySelector(navLinks[n].hash).scrollIntoView({
-          behavior: 'smooth',
-        })
-      })
-    }
-  }
-}
+import SmoothScroll from 'smooth-scroll'
+import elements from './elements'
 
 let addToggle = function () {
   const navbar = document.getElementById('navbar')
@@ -30,55 +18,118 @@ let addToggle = function () {
 
 window.addEventListener('resize', addToggle)
 
-const spyScrolling = () => {
-  const sections = document.querySelectorAll('section')
+const contactUsPosition = elements.contactUsSection.getBoundingClientRect().top
+console.log(contactUsPosition)
 
-  window.onscroll = () => {
-    const navbar = document.getElementById('navbar')
-    const navbarToggle = document.querySelector('toogle-menu')
-    const paddingTopNav = parseInt(
-      window.getComputedStyle(navbar, null).getPropertyValue('padding-top')
-    )
-    if (screen.width > 1480) {
-      if (window.pageYOffset < 223) {
-        navbar.style.paddingTop = 258 - window.pageYOffset + 'px'
-      } else if (paddingTopNav != 35) {
-        navbar.style.paddingTop = '35px'
-      }
-    } else if (screen.width < 768) {
-      if (window.pageYOffset < 18) {
-        navbar.style.paddingTop = 18 - window.pageYOffset + 'px'
-      } else if (paddingTopNav != 0) {
-        navbar.style.paddingTop = '0'
-      }
-    } else {
-      if (window.pageYOffset < 28) {
-        navbar.style.paddingTop = 28 - window.pageYOffset + 'px'
-      } else if (paddingTopNav != 0) {
-        navbar.style.paddingTop = '0'
-      }
+window.onscroll = () => {
+  const navbar = document.getElementById('navbar')
+  const navbarToggle = document.querySelector('toogle-menu')
+  const paddingTopNav = parseInt(
+    window.getComputedStyle(navbar, null).getPropertyValue('padding-top')
+  )
+
+  if (screen.width > 1480) {
+    // if (window.pageYOffset < 223) {
+    navbar.style.paddingTop = '258px'
+    // } else if (paddingTopNav != 35) {
+    //   navbar.style.paddingTop = '35px'
+    // }
+  } else if (window.width < 768 || screen.width < 768) {
+    if (window.pageYOffset < 18) {
+      navbar.style.paddingTop = 18 - window.pageYOffset + 'px'
+    } else if (paddingTopNav != 0) {
+      navbar.style.paddingTop = '0'
     }
+  } else {
+    if (window.pageYOffset < 28) {
+      navbar.style.paddingTop = 28 - window.pageYOffset + 'px'
+    } else if (paddingTopNav != 0) {
+      navbar.style.paddingTop = '0'
+    }
+  }
 
-    const scrollPos =
-      document.documentElement.scrollTop || document.body.scrollTop
-    for (let s in sections)
-      if (sections.hasOwnProperty(s) && sections[s].offsetTop <= scrollPos) {
-        const id = sections[s].id
-        if (id === 'about_us' || id === 'technology' || id === 'customers') {
-          document.getElementById('navbar').classList.add('navbar--light')
-        } else {
-          document.getElementById('navbar').classList.remove('navbar--light')
-        }
-
-        document
-          .querySelector('.navbar-collapse__item--active')
-          .classList.remove('navbar-collapse__item--active')
-        document
-          .querySelector(`a[href*=${id}`)
-          .parentNode.classList.add('navbar-collapse__item--active')
-      }
+  if (pageYOffset + 250 >= contactUsPosition) {
+    elements.navbar.classList.add('display-hide')
+  } else {
+    elements.navbar.classList.remove('display-hide')
   }
 }
 
-makeNavLinksSmooth()
-spyScrolling()
+// const spyScrolling = () => {
+//   const sections = document.querySelectorAll('section')
+
+//   window.onscroll = () => {
+//     const navbar = document.getElementById('navbar')
+//     const navbarToggle = document.querySelector('toogle-menu')
+//     const paddingTopNav = parseInt(
+//       window.getComputedStyle(navbar, null).getPropertyValue('padding-top')
+//     )
+//     if (screen.width > 1480) {
+//       // if (window.pageYOffset < 223) {
+//         navbar.style.paddingTop = '258px'
+//       // } else if (paddingTopNav != 35) {
+//       //   navbar.style.paddingTop = '35px'
+//       // }
+//     } else if (screen.width < 768) {
+//       if (window.pageYOffset < 18) {
+//         navbar.style.paddingTop = 18 - window.pageYOffset + 'px'
+//       } else if (paddingTopNav != 0) {
+//         navbar.style.paddingTop = '0'
+//       }
+//     } else {
+//       if (window.pageYOffset < 28) {
+//         navbar.style.paddingTop = 28 - window.pageYOffset + 'px'
+//       } else if (paddingTopNav != 0) {
+//         navbar.style.paddingTop = '0'
+//       }
+//     }
+
+//     const scrollPos =
+//       document.documentElement.scrollTop || document.body.scrollTop
+//     for (let s in sections)
+//       if (sections.hasOwnProperty(s) && sections[s].offsetTop <= scrollPos) {
+//         const id = sections[s].id
+//         if (id === 'about_us' || id === 'technology' || id === 'customers') {
+//           document.getElementById('navbar').classList.add('navbar--light')
+//         } else {
+//           document.getElementById('navbar').classList.remove('navbar--light')
+//         }
+
+//         document
+//           .querySelector('.navbar-collapse__item--active')
+//           .classList.remove('navbar-collapse__item--active')
+//         document
+//           .querySelector(`a[href*=${id}`)
+//           .parentNode.classList.add('navbar-collapse__item--active')
+//       }
+//   }
+// }
+
+new SmoothScroll('[data-scroll]', {
+  speed: 1000,
+  speedAsDuration: true,
+})
+const sections = [...elements.sections]
+
+const sectionsTop = Array.prototype.map.call(sections, (section) => {
+  return { id: section.id, offsetTop: section.offsetTop }
+})
+
+window.addEventListener('scroll', () => {
+  const ACTIVE_CLASSNAME = 'navbar-collapse__link--active'
+  const currScrollPosition =
+    document.documentElement.scrollTop || document.body.scrollTop
+
+  sectionsTop.forEach(({ id, offsetTop }) => {
+    if (offsetTop <= currScrollPosition) {
+      console.log('object')
+      document
+        .querySelector(`.${ACTIVE_CLASSNAME}`)
+        .classList.remove(ACTIVE_CLASSNAME)
+
+      document
+        .querySelector(`a[href*='${id}']`)
+        .parentElement.classList.add(ACTIVE_CLASSNAME)
+    }
+  })
+})
